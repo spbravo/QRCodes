97cbcb33f96adea85566b934f85d73993f7b50dd
spbravo <spbravo@gmail.com>
2020-09-27 18:59:38 +0200

    first commit

---
+++ QRCodes.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+	"os"
+
+	"github.com/skip2/go-qrcode"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "usage: QRCodes Something-to-Codify\n")
+		os.Exit(1)
+	}
+	//var png []byte
+	//png, err := qrcode.Encode("https://example.org", qrcode.Medium, 256)
+
+	//err := qrcode.WriteFile("https://www.google.es", qrcode.Medium, 256, "qr1.png")
+	err := qrcode.WriteColorFile(os.Args[1], qrcode.Medium, 256, color.Black, color.White, "qr1.png")
+	if err != nil {
+		fmt.Println("Ha habido un error %v", err)
+	}
+	//err := qrcode.WriteColorFile("https://example.org", qrcode.Medium, 256, color.Black, color.White, "qr1.png")
+
+}
